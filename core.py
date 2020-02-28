961f631c12674ffdff409d40e7ca8c21fe913f2c
sahil sodhi <sahilsinghsodhi@gmail.com>
2020-02-27 23:24:14 -0500

    Created knowledgebase. Added queries for the knowledgebase. Formatted query results.

--- core.py
+++ core.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
-import requests, pprint
+import requests
 import pandas as pd
-from rdflib import Graph, Literal, Namespace, RDFS, URIRef, BNode
+from rdflib import Graph, Literal, Namespace
 import rdflib.namespace as RDFnamespace
 import urllib.parse
+import time
+
 focudata = Namespace("http://focu.io/data#")
 focu = Namespace("http://focu.io/schema#")
 dbo = Namespace("http://dbpedia.org/ontology/")
+dbpedia = Namespace("http://dbpedia.org/resource/")
+
 
 class core:
     courseSubject = []
@@ -40,57 +44,51 @@ class core:
                         self.courseNumber.append(x[1])
                         self.courseName.append(x[2])
                     count = count + 1
-        df = pd.DataFrame({'Course Subject': self.courseSubject, 'Course Number': self.courseNumber, 'Course Name': self.courseName,
-                           'Course Description': self.courseDescription})
+        df = pd.DataFrame(
+            {'Course Subject': self.courseSubject, 'Course Number': self.courseNumber, 'Course Name': self.courseName,
+             'Course Description': self.courseDescription})
         df.to_csv('dataset/courses.csv', index=False, encoding='utf-8')
-
 
     def create_graph_schema(self):
         """Creating the basic schema of the graph. Adding the classes, properties and subclasses required
         for the university courses and students"""
-        g = Graph();
+        g = Graph()
         g.bind('focudata', focudata)
         g.bind('focu', focu)
         g.bind('dbo', dbo)
-        g.bind('foaf',RDFnamespace.FOAF)
-
+        g.bind('dbpedia', dbpedia)
+        g.bind('foaf', RDFnamespace.FOAF)
 
         ##Classes
-        #Person Class
-        g.add((RDFnamespace.FOAF.Person,RDFnamespace.RDF.type,RDFnamespace.RDFS.Class))
-        #Organization Class
-        g.add((RDFnamespace.FOAF.Organization,RDFnamespace.RDF.type,RDFnamespace.RDFS.Class))
+        # Person Class
+        g.add((RDFnamespace.FOAF.Person, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
+        # Organization Class
+        g.add((RDFnamespace.FOAF.Organization, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
 
-        #Student Class
-        g.add((focu.Student,RDFnamespace.RDF.type,RDFnamespace.RDFS.Class))
-        g.add((focu.Student,RDFnamespace.RDFS.subClassOf,RDFnamespace.FOAF.Person))
-        g.add((focu.Student,RDFnamespace.RDFS.label,Literal("StudentClass")))
-        g.add((focu.Student,RDFnamespace.RDFS.comment,Literal("This is a Student Class")))
+        # Student Class
+        g.add((focu.Student, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
+        g.add((focu.Student, RDFnamespace.RDFS.subClassOf, RDFnamespace.FOAF.Person))
+        g.add((focu.Student, RDFnamespace.RDFS.label, Literal("StudentClass")))
+        g.add((focu.Student, RDFnamespace.RDFS.comment, Literal("This is a Student Class")))
 
-        #Course Class
+        # Course Class
         g.add((focu.Course, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
         g.add((focu.Course, RDFnamespace.RDFS.label, Literal("University Courses")))
         g.add((focu.Course, RDFnamespace.RDFS.comment, Literal("This is a Course Class")))
 
-        #Topic Class
+        # Topic Class
         g.add((focu.Topic, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
-        g.add((focu.Topic, RDFnamespace.RDFS.label, Literal("Course Topics")))
-        g.add((focu.Topic, RDFnamespace.RDFS.comment, Literal("This is a Course Topic Class")))
+        g.add((focu.Topic, RDFnamespace.RDFS.label, Literal("Course Topic")))
+        g.add((focu.Topic, RDFnamespace.RDFS.comment, Literal("Topic extracted for a given course")))
 
         # University Class
         g.add((focu.University, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
         g.add((focu.University, RDFnamespace.RDFS.subClassOf, RDFnamespace.FOAF.Organization))
         g.add((focu.University, RDFnamespace.RDFS.label, Literal("Univeristy")))
 
-        # Course Grade Class
-        g.add((focu.Course_Grade, RDFnamespace.RDF.type, RDFnamespace.RDFS.Class))
-        g.add((focu.Course_Grade, RDFnamespace.RDFS.label, Literal("Course Grades")))
-        g.add((focu.Course_Grade, RDFnamespace.RDFS.comment, Literal("Grades for Courses")))
-
-
         ##Properties
 
-        #Course Name
+        # Course Name
         g.add((focu.course_name, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
         g.add((focu.course_name, RDFnamespace.RDFS.label, Literal("Course Name")))
         g.add((focu.course_name, RDFnamespace.RDFS.comment, Literal("Course Name")))
@@ -125,17 +123,56 @@ class core:
         g.add((focu.student_id, RDFnamespace.RDFS.domain, focu.Student))
         g.add((focu.student_id, RDFnamespace.RDFS.range, RDFnamespace.XSD.integer))
 
-        #Completed Courses
+        # Completed Courses
         g.add((focu.completed_course, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
         g.add((focu.completed_course, RDFnamespace.RDFS.label, Literal("Completed Courses by the Student")))
         g.add((focu.completed_course, RDFnamespace.RDFS.comment, Literal("Completed Courses by the Student")))
         g.add((focu.completed_course, RDFnamespace.RDFS.domain, focu.Student))
         g.add((focu.completed_course, RDFnamespace.RDFS.range, focu.Course_Grade))
 
+        # Graded Courses
+        g.add((focu.graded_courses, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.graded_courses, RDFnamespace.RDFS.label, Literal("grade for course", lang="en")))
+        g.add((focu.graded_courses, RDFnamespace.RDFS.comment, Literal("Course graded for a student")))
+        g.add((focu.graded_courses, RDFnamespace.RDFS.domain, focu.Student))
+        g.add((focu.graded_courses, RDFnamespace.RDFS.range, focu.Course))
 
-        g.serialize(format = 'turtle',destination='knowledge_base/ab.ttl')
+        # Subject Contains Topics
+        g.add((focu.contains, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.contains, RDFnamespace.RDFS.label, Literal("extracted topics", lang="en")))
+        g.add((focu.contains, RDFnamespace.RDFS.comment, Literal("Topics extracted from course description")))
+        g.add((focu.contains, RDFnamespace.RDFS.domain, focu.Course))
+        g.add((focu.contains, RDFnamespace.RDFS.range, RDFnamespace.XSD.string))
 
+        # Topics from Courses
+        g.add((focu.containInverse, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.containInverse, RDFnamespace.RDFS.label, Literal("extracted courses", lang="en")))
+        g.add((focu.containInverse, RDFnamespace.RDFS.comment, Literal("Courses extracted from Topics")))
+        g.add((focu.containInverse, RDFnamespace.RDFS.domain, focu.Topic))
+        g.add((focu.containInverse, RDFnamespace.RDFS.range, focu.Course))
 
+        # Course Term
+        g.add((focu.course_term, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.course_term, RDFnamespace.RDFS.label, Literal("course term", lang="en")))
+        g.add((focu.course_term, RDFnamespace.RDFS.comment, Literal("The term in which the given course was taken")))
+        g.add((focu.course_term, RDFnamespace.RDFS.domain, focu.Course))
+        g.add((focu.course_term, RDFnamespace.RDFS.range, RDFnamespace.XSD.string))
+
+        # Course Grade
+        g.add((focu.course_grade, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.course_grade, RDFnamespace.RDFS.label, Literal("course grade", lang="en")))
+        g.add((focu.course_grade, RDFnamespace.RDFS.comment, Literal("The grade received for a course")))
+        g.add((focu.course_grade, RDFnamespace.RDFS.domain, focu.Course))
+        g.add((focu.course_grade, RDFnamespace.RDFS.range, RDFnamespace.XSD.string))
+
+        # Student Identification
+        g.add((focu.id_number, RDFnamespace.RDF.type, RDFnamespace.RDF.Property))
+        g.add((focu.id_number, RDFnamespace.RDFS.label, Literal("Student Identification", lang="en")))
+        g.add((focu.id_number, RDFnamespace.RDFS.comment, Literal("Identification of the Student")))
+        g.add((focu.id_number, RDFnamespace.RDFS.domain, focu.Student))
+        g.add((focu.id_number, RDFnamespace.RDFS.range, RDFnamespace.XSD.integer))
+
+        g.serialize(format='turtle', destination='knowledge_base/ab.ttl')
 
     def create_graph_from_csv(self):
         """Creating rdf graph from the Dataset"""
@@ -144,24 +181,53 @@ class core:
         g = Graph()
         g.parse('knowledge_base/ab.ttl', format='n3')
 
-
         ##Adding Courses to the Graph
         df = pd.read_csv('dataset/courses.csv')
         self.courseSubject = df['Course Subject']
         self.courseName = df['Course Name']
         self.courseDescription = df['Course Description']
         self.courseNumber = df['Course Number']
-
-
-        for (course_num,course_subject, course_name, course_desc) in zip(self.courseNumber, self.courseSubject, self.courseName, self.courseDescription):
+        for (course_num, course_subject, course_name, course_desc) in zip(self.courseNumber, self.courseSubject,
+                                                                          self.courseName, self.courseDescription):
             course_name_data = urllib.parse.quote_plus(course_name)
+            course_name_data = course_name_data.replace("+","_")
             g.add((focudata[course_name_data], RDFnamespace.RDF.type, focu.Course))
             g.add((focudata.term(course_name_data), focu.course_name, Literal(course_name)))
             g.add((focudata.term(course_name_data), focu.course_subject, Literal(course_subject)))
             g.add((focudata.term(course_name_data), focu.course_number, Literal(course_num)))
             g.add((focudata.term(course_name_data), focu.course_description, Literal(course_desc)))
-            g.add((focudata.term(course_name_data), RDFS.seeAlso, Literal('Nothing to see ')))
-
+            # g.add((focudata.term(course_name_data), RDFS.seeAlso, Literal('Nothing to see ')))
+            url = urllib.parse.quote(str(course_desc))
+            url = 'https://api.dbpedia-spotlight.org/en/annotate?text=' + url
+            responseCounter = 0
+            while responseCounter < 3:
+                response = requests.get(url, headers={'accept': 'application/json'})
+                if response.status_code == 200:
+                    data = response.json()
+                    key = "Resources"
+                    # data = response.json()
+                    # print(json.dumps(data, indent=4))
+                    # print(course_desc)
+                    if key in data.keys():
+                        resources = data["Resources"]
+                        for res in resources:
+                            if res is None:
+                                continue
+                            else:
+                                topic = res["@URI"]
+                                englishName = res["@surfaceForm"]
+                                print(topic)
+                                g.add((focudata.term(course_name_data), focu.contains, focudata[topic]))
+                                g.add((focudata[topic], RDFnamespace.RDF.type, focu.Topic))
+                                g.add((focudata[topic], focu.containInverse, focudata.term(course_name_data)))
+                                g.add((focudata[topic], RDFnamespace.FOAF.name, Literal(englishName)))
+                    break
+                responseCounter = responseCounter + 1
+                print("Retrying..." + str(responseCounter) + " for : " + str(response))
+                if responseCounter == 3:
+                    time.sleep(5)
+                else:
+                    time.sleep(1)
 
         ###Adding Student information to the Graph
         df_stu = pd.read_csv('dataset/student_dataset.csv')
@@ -171,9 +237,14 @@ class core:
         student_email = df_stu['Email']
         some_courses_from_list = self.courseName[0:30]
 
-        for (student_first_name,student_last_name, student_ID, student_email,courses) in zip(student_first_name,student_last_name, student_ID, student_email,some_courses_from_list):
-            student_name = urllib.parse.quote_plus(student_first_name+" "+student_last_name)
+        for (student_first_name, student_last_name, student_ID, student_email, courses) in zip(student_first_name,
+                                                                                               student_last_name,
+                                                                                               student_ID,
+                                                                                               student_email,
+                                                                                               some_courses_from_list):
+            student_name = urllib.parse.quote_plus(student_first_name + " " + student_last_name)
             course_name_data = urllib.parse.quote_plus(courses)
+            course_name_data = course_name_data.replace("+","_")
             g.add((focudata[student_name], RDFnamespace.RDF.type, focu.Student))
             g.add((focudata.term(student_name), RDFnamespace.FOAF.firstName, Literal(student_first_name)))
             g.add((focudata.term(student_name), RDFnamespace.FOAF.lastName, Literal(student_last_name)))
@@ -181,14 +252,5 @@ class core:
             g.add((focudata.term(student_name), RDFnamespace.FOAF.mbox, Literal(student_email)))
             g.add((focudata.term(student_name), focu.completed_course, focudata[course_name_data]))
 
-
-
-
-        ##making a ttl from the graph
-        g.serialize(destination='knowledge_base/ab.ttl',format='ttl')
-
-        # for s, p, o in g.triples((None, focu.course_description, None)):
-        #     print( "%s is a %s" % (s, o))
-
-
-
+        # making a ttl from the graph
+        g.serialize(destination='knowledge_base/ab.ttl', format='ttl')
--- main.py
+++ main.py
@@ -4,5 +4,3 @@ p1 = core.core()
 p1.get_course_html_to_csv()
 p1.create_graph_schema()
 p1.create_graph_from_csv()
-
-
